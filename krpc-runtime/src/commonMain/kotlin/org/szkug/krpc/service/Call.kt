60cf231b39cec7129f77680776950ece62bcec0f
korilin <korilin.dev@gmail.com>
2024-05-02 09:50:35 +0800

    fix: call function return

--- krpc-runtime/src/commonMain/kotlin/org/szkug/krpc/service/Call.kt
+++ krpc-runtime/src/commonMain/kotlin/org/szkug/krpc/service/Call.kt
@@ -2,5 +2,5 @@ package org.szkug.krpc.service
 
 interface Call {
 
-    operator fun invoke(serviceName: String, functionName: String, requestData: ByteArray)
+    operator fun invoke(serviceName: String, functionName: String, requestData: ByteArray): ByteArray
 }
