5673811314cc19e7637e568f3071b8a32390b6a5
korilin <korilin.dev@gmail.com>
2024-05-04 03:58:18 +0800

    fix: test for iosX64Target

--- wire-schema/src/main/kotlin/KrpcServiceGenerator.kt
+++ wire-schema/src/main/kotlin/KrpcServiceGenerator.kt
@@ -122,10 +122,10 @@ class KrpcServiceGenerator(
                  *     override fun get(call): Service = ServiceImpl(call)
                  * }
                  */
-                val createFunction = FunSpec.builder(ServiceFactory<*>::create.name).apply {
+                val createFunction = FunSpec.builder(ServiceFactory::create.name).apply {
                     addModifiers(KModifier.OVERRIDE)
                     addParameter(ParameterSpec("call", Call::class.asTypeName()))
-                    addStatement("return $name(call)")
+                    addStatement("return Unit")
                 }.build()
                 val factoryObject = TypeSpec.objectBuilder("Factory")
                     .addSuperinterface(ServiceFactory::class.asClassName().parameterizedBy(interfaceName))
